8ab088995c8806d8ebe8da00a87aefbecc26065f
recreanext <ann.b.diaz@gmail.com>
2025-07-08 19:32:13 -0300

    Delete src directory

--- src/app/api/login/route.js
+++
@@ -1,7 +0,0 @@
-export async function POST(request) {
-  const { password } = await request.json();
-  if (password === process.env.ADMIN_PASS) {
-    return new Response(JSON.stringify({ success: true }), { status: 200 });
-  }
-  return new Response(JSON.stringify({ success: false }), { status: 401 });
-}
